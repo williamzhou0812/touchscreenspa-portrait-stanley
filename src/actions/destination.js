80d97335cc6863af8f7cb48b6b86b7cf3b1cccec
Jonathan Adhitama Wongsodihardjo <jonathan@johnbatman.com.au>
2018-04-18 16:52:05 +1000

    Add services into Destination Explore

--- src/actions/destination.js
+++ src/actions/destination.js
@@ -7,12 +7,20 @@ import {
     eventNamespace,
     diningNamespace,
     activityNamespace,
-    DECIMAL_RADIX
+    DECIMAL_RADIX,
+    transportNamespace,
+    retailNamespace,
+    miningNamespace,
+    essentialNamespace
 } from '../Constants';
 import AccommodationIcon from '../MainTab/icons/ACCOMMODATION_ICON.png';
 import ActivitiesIcon from '../MainTab/icons/ACTIVITIES_ICON.png';
 import DiningIcon from '../MainTab/icons/DINING_ICON.png';
 import EventsIcon from '../MainTab/icons/EVENTS_ICON.png';
+import TransportIcon from '../Service/icons/TRANSPORT_ICON.png';
+import RetailIcon from "../Service/icons/RETAIL_ICON.png";
+import MiningIcon from "../Service/icons/MINING_ICON.png";
+import EssentialIcon from "../Service/icons/ESSENTIAL_ICON.png";
 
 export const fetchDestinationList = () => async dispatch => {
     const res = await axios.get(createURL('destination/'));
@@ -34,8 +42,6 @@ export const fetchDestinationDetail = (
     id,
     destinationList
 ) => async dispatch => {
-    console.log(id);
-    console.log(destinationList);
     let [data, responseStatus] = [null, 404];
     let toRender = [];
     const desDetailiD = parseInt(id, DECIMAL_RADIX);
@@ -50,52 +56,88 @@ export const fetchDestinationDetail = (
 
     data.eventDestination.forEach((item, _) => {
         const linkTo = eventNamespace + '/' + item.period + '/' + item.id;
-        const postLink = createURL(`eventpost/${item.id}/`);
         toRender.push({
             id: item.id,
             period: item.period,
             title: item.title,
             type: 'EVENT',
             linkTo: linkTo,
-            postLink: postLink,
             icon: EventsIcon
         });
     });
     data.accomodationDestination.forEach((item, _) => {
         const linkTo = accomodationNamespace + '/' + data.id + '/' + item.id;
-        const postLink = createURL(`accomodationpost/${item.id}/`);
         toRender.push({
             id: item.id,
             title: item.title,
             type: 'HOTEL',
             linkTo: linkTo,
-            postLink: postLink,
             icon: AccommodationIcon
         });
     });
     data.restaurantDestination.forEach((item, _) => {
         const linkTo = diningNamespace + '/' + item.id;
-        const postLink = createURL(`restaurantpost/${item.id}/`);
         toRender.push({
             id: item.id,
             title: item.title,
             type: 'DINING',
             linkTo: linkTo,
-            postLink: postLink,
             icon: DiningIcon
         });
     });
     data.activityDestinationDestination.forEach((item, _) => {
         const linkTo = activityNamespace + '/' + item.activity + '/' + item.id;
-        const postLink = createURL(`activitydestinationpost/${item.id}/`);
         toRender.push({
             id: item.id,
             title: `${item.activityTitle} IN ${item.title}`,
             activity: item.activity,
             type: 'ACTIVITIES',
             linkTo: linkTo,
-            postLink: postLink,
             icon: ActivitiesIcon
+        });
+    });
+    data.transportationDestination.forEach((item) => {
+        const linkTo = transportNamespace + '/' + item.serviceType + '/' + item.id;
+        toRender.push({
+            id: item.id,
+            title: item.title,
+            isBranch: item.isBranch,
+            type: 'CAR HIRE & TRANSPORT',
+            linkTo: linkTo,
+            icon: TransportIcon
+        });
+    });
+    data.retailDestination.forEach((item) => {
+        const linkTo = retailNamespace + '/' + item.serviceType + '/' + item.id;
+        toRender.push({
+            id: item.id,
+            title: item.title,
+            isBranch: item.isBranch,
+            type: 'RETAIL & SERVICES',
+            linkTo: linkTo,
+            icon: RetailIcon
+        });
+    });
+    data.miningDestination.forEach((item) => {
+        const linkTo = miningNamespace + '/' + item.serviceType + '/' + item.id;
+        toRender.push({
+            id: item.id,
+            title: item.title,
+            isBranch: item.isBranch,
+            type: 'MINING & RESOURCES',
+            linkTo: linkTo,
+            icon: MiningIcon
+        });
+    });
+    data.essentialServiceDestination.forEach((item) => {
+        const linkTo = essentialNamespace + '/' + item.serviceType + '/' + item.id;
+        toRender.push({
+            id: item.id,
+            title: item.title,
+            isBranch: item.isBranch,
+            type: 'ESSENTIAL SERVICES',
+            linkTo: linkTo,
+            icon: EssentialIcon
         });
     });
     dispatch({
