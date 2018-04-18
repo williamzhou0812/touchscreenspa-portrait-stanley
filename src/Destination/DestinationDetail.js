9402e02062fd8129fe6283aca47caa1affaa262a
Neal Shen <>
2018-04-18 14:15:10 +1000

    addd transition for destinations details and lists

--- src/Destination/DestinationDetail.js
+++ src/Destination/DestinationDetail.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import * as actions from '../actions/destination';
-import AllAreas from "./icons/AllAreas.png";
-import { HeavyOrange, destinationNamespace, HeavyBlue, MediumBlue, LightBlueButtonBackground, ExtraHeavyBlueGreen, imageGallery, LightOrange, MediumOrange } from "../Constants";
-import ExportList from './ExploreList'
+import AllAreas from './icons/AllAreas.png';
+import {
+    HeavyOrange,
+    destinationNamespace,
+    HeavyBlue,
+    MediumBlue,
+    LightBlueButtonBackground,
+    ExtraHeavyBlueGreen,
+    imageGallery,
+    LightOrange,
+    MediumOrange
+} from '../Constants';
+import ExportList from './ExploreList';
 import ExploreList from './ExploreList';
 
 class DestinationDetail extends React.Component {
@@ -12,13 +22,16 @@ class DestinationDetail extends React.Component {
         super(props);
         this.state = {
             explore: false
-        }
+        };
         this.goToExplore = this.goToExplore.bind(this);
         this.returnFromExplore = this.returnFromExplore.bind(this);
     }
     retrieveData() {
         const { destinations } = this.props.destinationList;
-        this.props.fetchDestinationDetail(this.props.match.params.id, destinations);
+        this.props.fetchDestinationDetail(
+            this.props.match.params.id,
+            destinations
+        );
     }
     componentDidMount() {
         this.retrieveData();
@@ -35,73 +48,230 @@ class DestinationDetail extends React.Component {
         this.setState({ explore: false });
     }
     styles = {
-        horizontalVerticalCenter: {display: "flex", alignItems: "center", justifyContent: "center"}
-    }
+        horizontalVerticalCenter: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }
+    };
     render() {
-        const { destination, exploreData, status } = this.props.destinationDetail;
+        const {
+            destination,
+            exploreData,
+            status
+        } = this.props.destinationDetail;
         const { explore } = this.state;
         if (explore) {
             return (
-                <div style={{width: "100%", height: "100%", display: "flex"}}>
-                    <div onClick={this.returnFromExplore} style={{backgroundColor: HeavyOrange, width: "14%", height: "100%", boxShadow: "9.899px 0px 7px 0px rgba(0,0,0,0.6)", zIndex: 1}}>
-                        <div style={{height: "14%"}}>
-                            <img src={AllAreas} style={{width: "33%"}} />
-                            <div style={{color: "white"}}>BACK TO OVERVIEW</div>
-                        </div>
-                    </div>
-                    {(!!destination && exploreData && status === 200) && (
-                        <div style={{width: "86%", height: "100%"}}>
-                            <div style={{height: "8%", backgroundColor: LightOrange, color: "white", letterSpacing: 5, ...this.styles.horizontalVerticalCenter, fontSize: "20pt"}}>{destination.title}</div>
-                            <div style={{height: "92%"}}>
-                                <ExploreList data={exploreData} />
+                <div
+                    style={{ width: '100%', height: '100%', display: 'flex' }}
+                    className="section--bottom--animation"
+                >
+                    <div
+                        onClick={this.returnFromExplore}
+                        style={{
+                            backgroundColor: HeavyOrange,
+                            width: '14%',
+                            height: '100%',
+                            boxShadow: '9.899px 0px 7px 0px rgba(0,0,0,0.6)',
+                            zIndex: 1
+                        }}
+                    >
+                        <div style={{ height: '14%' }}>
+                            <img src={AllAreas} style={{ width: '33%' }} />
+                            <div style={{ color: 'white' }}>
+                                BACK TO OVERVIEW
                             </div>
                         </div>
-                    )}
+                    </div>
+                    {!!destination &&
+                        exploreData &&
+                        status === 200 && (
+                            <div style={{ width: '86%', height: '100%' }}>
+                                <div
+                                    style={{
+                                        height: '8%',
+                                        backgroundColor: LightOrange,
+                                        color: 'white',
+                                        letterSpacing: 5,
+                                        ...this.styles.horizontalVerticalCenter,
+                                        fontSize: '20pt'
+                                    }}
+                                >
+                                    {destination.title}
+                                </div>
+                                <div style={{ height: '92%' }}>
+                                    <ExploreList data={exploreData} />
+                                </div>
+                            </div>
+                        )}
                 </div>
             );
         } else {
             return (
-                <div style={{width: "100%", height: "100%", display: "flex"}}>
-                    <div style={{backgroundColor: HeavyOrange, width: "14%", height: "100%", boxShadow: "9.899px 0px 7px 0px rgba(0,0,0,0.6)", zIndex: 1}}>
-                        <Link style={{height: "14%"}} to={destinationNamespace}>
-                            <img src={AllAreas} style={{width: "33%"}} />
-                            <div style={{color: "white"}}>ALL AREAS</div>
+                <div
+                    style={{ width: '100%', height: '100%', display: 'flex' }}
+                    className="section--bottom--animation"
+                >
+                    <div
+                        style={{
+                            backgroundColor: HeavyOrange,
+                            width: '14%',
+                            height: '100%',
+                            boxShadow: '9.899px 0px 7px 0px rgba(0,0,0,0.6)',
+                            zIndex: 1
+                        }}
+                    >
+                        <Link
+                            style={{ height: '14%' }}
+                            to={destinationNamespace}
+                        >
+                            <img src={AllAreas} style={{ width: '33%' }} />
+                            <div style={{ color: 'white' }}>ALL AREAS</div>
                         </Link>
-                        <div style={{fontSize: "28pt", transform: "rotate(-90deg)", color: "white", height: "86%", display: "flex", justifyContent: "center", alignItems: "center"}}>DESTINATIONS</div>
+                        <div
+                            style={{
+                                fontSize: '28pt',
+                                transform: 'rotate(-90deg)',
+                                color: 'white',
+                                height: '86%',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}
+                        >
+                            DESTINATIONS
+                        </div>
                     </div>
-    
-                    {(!!destination && status === 200) && (
-                        <div style={{width: "86%", height: "100%"}}>
-                            <div style={{height: "50%"}}>
-                                {destination.imageDestination.length > 1 ? (
-                                    <div>
-                                        {imageGallery(destination.imageDestination, "100%", "27vh")}
+
+                    {!!destination &&
+                        status === 200 && (
+                            <div style={{ width: '86%', height: '100%' }}>
+                                <div style={{ height: '50%' }}>
+                                    {destination.imageDestination.length > 1 ? (
+                                        <div>
+                                            {imageGallery(
+                                                destination.imageDestination,
+                                                '100%',
+                                                '27vh'
+                                            )}
+                                        </div>
+                                    ) : (
+                                        <img
+                                            src={
+                                                destination.imageDestination[0]
+                                                    .imageFile
+                                            }
+                                            style={{
+                                                width: '100%',
+                                                height: '100%'
+                                            }}
+                                        />
+                                    )}
+                                </div>
+                                <div
+                                    style={{
+                                        height: '50%',
+                                        backgroundColor: ExtraHeavyBlueGreen,
+                                        paddingLeft: 20,
+                                        color: 'white'
+                                    }}
+                                >
+                                    <div
+                                        style={{
+                                            height: '30%',
+                                            letterSpacing: '5px',
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            fontSize: '28pt'
+                                        }}
+                                    >
+                                        {destination.title}
                                     </div>
-                                ) : (
-                                    <img src={destination.imageDestination[0].imageFile} style={{width: "100%", height: "100%"}} />
-                                )}
-                            </div>
-                            <div style={{height: "50%", backgroundColor: ExtraHeavyBlueGreen, paddingLeft: 20, color: "white"}}>
-                                <div style={{height: "30%", letterSpacing: "5px", display: "flex", alignItems: "center", fontSize: "28pt"}}>{destination.title}</div>
-                                <div style={{height: "70%", display: "flex"}}>
-                                    <div style={{width: "50%", height: "100%"}}>
-                                        {destination.description}
-                                    </div>
-                                    <div style={{width: "50%", height: "100%", marginLeft: 10}}>
-                                        <div>PROVINCE: {destination.province}</div>
-                                        <div>CLOSEST AIRPORT: {destination.airport}</div>
-                                        <div onClick={this.goToExplore} style={{height: "28%", width: "80%", backgroundColor: HeavyOrange, marginLeft: 10, ...this.styles.horizontalVerticalCenter}}>EXPLORE</div>
-                                        {destination.mapDestination.length > 0 && (
-                                            <div style={{height: "17%", width: "80%", marginLeft: 10, backgroundColor: LightBlueButtonBackground, ...this.styles.horizontalVerticalCenter}}>{destination.mapDestination[0].title}</div>
-                                        )}
-                                        {destination.mapDestination.length > 1 && (
-                                            <div style={{height: "17%", width: "80%", marginLeft: 10, backgroundColor: LightBlueButtonBackground, ...this.styles.horizontalVerticalCenter}}>{destination.mapDestination[1].title}</div>
-                                        )}
+                                    <div
+                                        style={{
+                                            height: '70%',
+                                            display: 'flex'
+                                        }}
+                                    >
+                                        <div
+                                            style={{
+                                                width: '50%',
+                                                height: '100%'
+                                            }}
+                                        >
+                                            {destination.description}
+                                        </div>
+                                        <div
+                                            style={{
+                                                width: '50%',
+                                                height: '100%',
+                                                marginLeft: 10
+                                            }}
+                                        >
+                                            <div>
+                                                PROVINCE: {destination.province}
+                                            </div>
+                                            <div>
+                                                CLOSEST AIRPORT:{' '}
+                                                {destination.airport}
+                                            </div>
+                                            <div
+                                                onClick={this.goToExplore}
+                                                style={{
+                                                    height: '28%',
+                                                    width: '80%',
+                                                    backgroundColor: HeavyOrange,
+                                                    marginLeft: 10,
+                                                    ...this.styles
+                                                        .horizontalVerticalCenter
+                                                }}
+                                            >
+                                                EXPLORE
+                                            </div>
+                                            {destination.mapDestination.length >
+                                                0 && (
+                                                <div
+                                                    style={{
+                                                        height: '17%',
+                                                        width: '80%',
+                                                        marginLeft: 10,
+                                                        backgroundColor: LightBlueButtonBackground,
+                                                        ...this.styles
+                                                            .horizontalVerticalCenter
+                                                    }}
+                                                >
+                                                    {
+                                                        destination
+                                                            .mapDestination[0]
+                                                            .title
+                                                    }
+                                                </div>
+                                            )}
+                                            {destination.mapDestination.length >
+                                                1 && (
+                                                <div
+                                                    style={{
+                                                        height: '17%',
+                                                        width: '80%',
+                                                        marginLeft: 10,
+                                                        backgroundColor: LightBlueButtonBackground,
+                                                        ...this.styles
+                                                            .horizontalVerticalCenter
+                                                    }}
+                                                >
+                                                    {
+                                                        destination
+                                                            .mapDestination[1]
+                                                            .title
+                                                    }
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    )}
+                        )}
                 </div>
             );
         }
@@ -112,7 +282,7 @@ const mapStateToProps = ({ destinationList, destinationDetail }) => {
     return {
         destinationList,
         destinationDetail
-    }
-}
+    };
+};
 
 export default connect(mapStateToProps, actions)(DestinationDetail);
--- src/Destination/ExploreList.js
+++ src/Destination/ExploreList.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import UpButton from "./icons/UpExploreButton.png";
-import DownButton from "./icons/DownExploreButton.png";
-import { MediumOrange, shiftArray, ExtraHeavyBlueGreen, HeavyBlue } from "../Constants";
+import React from 'react';
+import UpButton from './icons/UpExploreButton.png';
+import DownButton from './icons/DownExploreButton.png';
+import {
+    MediumOrange,
+    shiftArray,
+    ExtraHeavyBlueGreen,
+    HeavyBlue
+} from '../Constants';
 
 class ExploreList extends React.Component {
     constructor(props) {
         super(props);
         const { data } = this.props;
         this.state = {
-            data,
-        }
+            data
+        };
         this.goUp = this.goUp.bind(this);
         this.goDown = this.goDown.bind(this);
     }
@@ -17,40 +22,107 @@ class ExploreList extends React.Component {
         let items = this.state.data.slice();
         items = shiftArray(items, 1);
         this.setState({
-            data: items,
+            data: items
         });
     }
     goDown() {
         let items = this.state.data.slice();
         items = shiftArray(items, -1);
         this.setState({
-            data: items,
+            data: items
         });
     }
     styles = {
-        horizontalVerticalCenter: {display: "flex", alignItems: "center", justifyContent: "center"}
-    }
+        horizontalVerticalCenter: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }
+    };
     render() {
         const { data } = this.state;
-        const itemHeight = this.props.data.length >= 13 ? "7.7%" : `${100 / this.props.data.length}%`;
+        const itemHeight =
+            this.props.data.length >= 13
+                ? '7.7%'
+                : `${100 / this.props.data.length}%`;
         return (
-            <div style={{width: "100%", height: "100%"}}>
-                <div style={{height: "6%", backgroundColor: MediumOrange, ...this.styles.horizontalVerticalCenter}} onClick={this.goUp}>
-                    <img src={UpButton} style={{width: "5%"}} />
+            <div
+                style={{ width: '100%', height: '100%' }}
+                className="section--bottom--animation"
+            >
+                <div
+                    style={{
+                        height: '6%',
+                        backgroundColor: MediumOrange,
+                        ...this.styles.horizontalVerticalCenter
+                    }}
+                    onClick={this.goUp}
+                >
+                    <img src={UpButton} style={{ width: '5%' }} />
                 </div>
-                <div style={{height: "88%", overflow: "hidden"}}>
-                    <div style={{height: "100%", overflowY: "auto", marginRight: "-30px"}}>
-                        <div style={{height: "100%", overflow: "auto", paddingRight: "30px"}}>
+                <div style={{ height: '88%', overflow: 'hidden' }}>
+                    <div
+                        style={{
+                            height: '100%',
+                            overflowY: 'auto',
+                            marginRight: '-30px'
+                        }}
+                    >
+                        <div
+                            style={{
+                                height: '100%',
+                                overflow: 'auto',
+                                paddingRight: '30px'
+                            }}
+                        >
                             {data.map((item, index) => {
-                                return(
-                                    <div key={`${item.id}-${index}`} style={{height: itemHeight, color: "white", display: "flex"}}>
-                                        <div style={{width: "33%", backgroundColor: ExtraHeavyBlueGreen, display: "flex"}}>
-                                            <div style={{width: "32%", ...this.styles.horizontalVerticalCenter}}>
-                                                <img src={item.icon} style={{width: "43%"}} />
+                                return (
+                                    <div
+                                        key={`${item.id}-${index}`}
+                                        style={{
+                                            height: itemHeight,
+                                            color: 'white',
+                                            display: 'flex'
+                                        }}
+                                    >
+                                        <div
+                                            style={{
+                                                width: '33%',
+                                                backgroundColor: ExtraHeavyBlueGreen,
+                                                display: 'flex'
+                                            }}
+                                        >
+                                            <div
+                                                style={{
+                                                    width: '32%',
+                                                    ...this.styles
+                                                        .horizontalVerticalCenter
+                                                }}
+                                            >
+                                                <img
+                                                    src={item.icon}
+                                                    style={{ width: '43%' }}
+                                                />
                                             </div>
-                                            <div style={{width: "68%", ...this.styles.horizontalVerticalCenter}}>{item.type}</div>
+                                            <div
+                                                style={{
+                                                    width: '68%',
+                                                    ...this.styles
+                                                        .horizontalVerticalCenter
+                                                }}
+                                            >
+                                                {item.type}
+                                            </div>
                                         </div>
-                                        <div style={{width: "67%", backgroundColor: HeavyBlue, display: "flex", alignItems: "center", paddingLeft: 20}}>
+                                        <div
+                                            style={{
+                                                width: '67%',
+                                                backgroundColor: HeavyBlue,
+                                                display: 'flex',
+                                                alignItems: 'center',
+                                                paddingLeft: 20
+                                            }}
+                                        >
                                             {item.title.toUpperCase()}
                                         </div>
                                     </div>
@@ -59,8 +131,15 @@ class ExploreList extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div style={{height: "6%", backgroundColor: MediumOrange, ...this.styles.horizontalVerticalCenter}} onClick={this.goDown}>
-                    <img src={DownButton} style={{width: "5%"}} />
+                <div
+                    style={{
+                        height: '6%',
+                        backgroundColor: MediumOrange,
+                        ...this.styles.horizontalVerticalCenter
+                    }}
+                    onClick={this.goDown}
+                >
+                    <img src={DownButton} style={{ width: '5%' }} />
                 </div>
             </div>
         );
