7915cd1c7ca7882132feae44712208f9d0c13f01
Jonathan Adhitama Wongsodihardjo <jonathan@johnbatman.com.au>
2018-04-24 09:50:49 +1000

    Render greeting content and managing director

--- src/Airport/AirportInfo.js
+++ src/Airport/AirportInfo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from "react-redux";
 import Modal from "react-bootstrap-modal";
-import { HeavyOrange } from '../Constants';
+import { HeavyOrange, ExtraHeavyBlueGreen } from '../Constants';
 
 class AirportInfo extends React.Component {
     constructor(props) {
@@ -38,7 +38,20 @@ class AirportInfo extends React.Component {
                         <div>WELCOME TO</div>
                         <div style={{fontFamily: "The Braggest", fontSize: "30pt"}}>PORT MORESBY AIRPORT</div>
                     </div>
-                    <div style={{height: "40%"}}></div>
+                    <div style={{height: "40%", backgroundColor: ExtraHeavyBlueGreen}}>
+                        <div style={{height: "75%", display: "flex"}}>
+                            <div style={{flexBasis: "66%"}}>
+                                <div>GREETINGS {"&"} WELCOME</div>
+                                <div>On behalf of the Minister of Civil Aviation and the Board of Directors I welcome you to the National Airports Corporation. We are grateful that you have taken time to read about who we are, what we are doing and what we plan to do. NAC owns and operate 22 Airports across Papua New Guinea. We are committed to providing you a seamless travel experience through our airports. Like any other industry, this is a critical time for NAC given the challenges posed by technology and increased customer expectations. Our job is to ensure that NAC thrives despite those challenges, and deliver expectations held by our stakeholders.</div>
+                            </div>
+                            <div style={{flexBasis: "34%", ...this.styles.horizontalVerticalCenter, flexDirection: "column"}}>
+                                <img src={airport.directorImage} style={{height: "50%"}} />
+                                <div>{airport.directorName}</div>
+                                <div>Managing Director {"&"} CEO</div>
+                            </div>
+                        </div>
+                        <div style={{height: "25%"}}></div>
+                    </div>
                 </div>
             </div>
         ); 
