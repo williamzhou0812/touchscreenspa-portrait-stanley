2a82daca762d4d156f03f0609fc870e48c41c173
Jonathan Adhitama Wongsodihardjo <jonathan@johnbatman.com.au>
2018-04-26 12:54:19 +1000

    Fix incorrect logic in selecting a random image and separating it between other images in airport action

--- src/actions/airport.js
+++ src/actions/airport.js
@@ -5,15 +5,12 @@ import axios from "axios";
 export const fetchAirportDetail = () => async dispatch => {
     const res = await axios.get(createURL('airport/1/'));
     const airportInfoMainImage = getRandomImage(res.data.imageAirport, false);
-    const mainImageIndex = res.data.imageAirport.findIndex(image => {
-        return image.id = airportInfoMainImage.id;
-    });
     dispatch({
         type: AIRPORT_INFO,
         payload: {
             airport: res.data,
             mainImage: airportInfoMainImage.imageFile,
-            otherImages: [...res.data.imageAirport.slice(0, mainImageIndex), ...res.data.imageAirport.slice(mainImageIndex + 1)],
+            otherImages: res.data.imageAirport.filter(image => {return image.id !== airportInfoMainImage.id}),
             status: res.status
         }
     });
