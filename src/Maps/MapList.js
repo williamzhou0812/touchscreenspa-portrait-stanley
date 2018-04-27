7a88bc4fd4fa73604ab6091a250da15ebaa8125b
Neal Shen <>
2018-04-27 15:16:24 +1000

    added anmiation for map section

--- src/Maps/MapList.js
+++ src/Maps/MapList.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 import UpButton from '../Destination/icons/UpExploreButton.png';
 import DownButton from '../Destination/icons/DownExploreButton.png';
 import {
@@ -14,7 +14,7 @@ import {
     MAP_HEIGHT,
     MAP_WIDTH
 } from '../Constants';
-import { Modal } from "react-bootstrap";
+import { Modal } from 'react-bootstrap';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import CloseIcon from 'material-ui/svg-icons/navigation/close';
 import ReactImageMagnify from 'react-image-magnify';
@@ -29,7 +29,7 @@ class MapList extends React.Component {
         });
         this.state = {
             data
-        }
+        };
         this.goUp = this.goUp.bind(this);
         this.goDown = this.goDown.bind(this);
     }
@@ -51,7 +51,7 @@ class MapList extends React.Component {
         this.setState({
             data: [
                 ...this.state.data.slice(0, index),
-                {...this.state.data[index], map: true},
+                { ...this.state.data[index], map: true },
                 ...this.state.data.slice(index + 1)
             ]
         });
@@ -60,7 +60,7 @@ class MapList extends React.Component {
         this.setState({
             data: [
                 ...this.state.data.slice(0, index),
-                {...this.state.data[index], map: false},
+                { ...this.state.data[index], map: false },
                 ...this.state.data.slice(index + 1)
             ]
         });
@@ -76,7 +76,10 @@ class MapList extends React.Component {
         const { data } = this.state;
         const itemHeight = `${100 / SUBSECTION_LIST_ENTRIES}%`;
         return (
-            <div style={{ width: '100%', height: '100%', display: 'flex' }}>
+            <div
+                className="section--bottom--animation"
+                style={{ width: '100%', height: '100%', display: 'flex' }}
+            >
                 <div
                     style={{
                         backgroundColor: HeavyOrange,
@@ -99,7 +102,9 @@ class MapList extends React.Component {
                             letterSpacing: '10px'
                         }}
                     >
-                        <span style={{transform: 'rotate(-90deg)'}}>MAPS</span>
+                        <span style={{ transform: 'rotate(-90deg)' }}>
+                            MAPS
+                        </span>
                     </div>
                 </div>
                 <div style={{ width: '86%' }}>
@@ -141,92 +146,162 @@ class MapList extends React.Component {
                                 }}
                             >
                                 {data.map((item, index) => {
-                                    const isLastItem = index === data.length - 1;
-                                        return (
-                                            <div style={{height: itemHeight}} key={`${item.id}-${index}`}>
+                                    const isLastItem =
+                                        index === data.length - 1;
+                                    return (
+                                        <div
+                                            style={{ height: itemHeight }}
+                                            key={`${item.id}-${index}`}
+                                        >
+                                            <div
+                                                style={{
+                                                    height: '100%',
+                                                    color: 'white',
+                                                    display: 'flex'
+                                                }}
+                                                onClick={() =>
+                                                    this.openMap(index)
+                                                }
+                                            >
                                                 <div
                                                     style={{
-                                                        height: "100%",
-                                                        color: 'white',
-                                                        display: 'flex'
+                                                        width: '33%',
+                                                        backgroundImage: `url(${
+                                                            item.mapImage
+                                                        })`,
+                                                        backgroundSize: 'cover',
+                                                        backgroundPosition:
+                                                            'center',
+                                                        borderBottom: isLastItem
+                                                            ? 'none'
+                                                            : `1px solid ${LightBlue}`
                                                     }}
-                                                    onClick={() => this.openMap(index)}
+                                                />
+                                                <div
+                                                    style={{
+                                                        width: '67%',
+                                                        backgroundColor: HeavyBlue,
+                                                        display: 'flex',
+                                                        alignItems: 'center',
+                                                        paddingLeft: 35,
+                                                        fontSize: '24px',
+                                                        letterSpacing: '3px',
+                                                        borderBottom: isLastItem
+                                                            ? 'none'
+                                                            : '1px solid rgb(183,223,228)'
+                                                    }}
                                                 >
                                                     <div
                                                         style={{
-                                                            width: '33%',
-                                                            backgroundImage: `url(${item.mapImage})`,
-                                                            backgroundSize:
-                                                                'cover',
-                                                            backgroundPosition:
-                                                                'center',
-                                                            borderBottom: isLastItem
-                                                                ? 'none'
-                                                                : `1px solid ${LightBlue}`
-                                                        }}
-                                                    />   
-                                                    <div
-                                                        style={{
-                                                            width: '67%',
-                                                            backgroundColor: HeavyBlue,
+                                                            height: '100%',
                                                             display: 'flex',
-                                                            alignItems: 'center',
-                                                            paddingLeft: 35,
-                                                            fontSize: '24px',
-                                                            letterSpacing: '3px',
-                                                            borderBottom: isLastItem
-                                                                ? 'none'
-                                                                : '1px solid rgb(183,223,228)'
+                                                            flexDirection:
+                                                                'column',
+                                                            justifyContent:
+                                                                'center',
+                                                            alignItems: 'center'
                                                         }}
                                                     >
-                                                        <div style={{height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                                                            {item.title.toUpperCase()}
-                                                            {(item.province && item.id !== 1) && (
-                                                                <div>{item.province}</div>
+                                                        {item.title.toUpperCase()}
+                                                        {item.province &&
+                                                            item.id !== 1 && (
+                                                                <div>
+                                                                    {
+                                                                        item.province
+                                                                    }
+                                                                </div>
                                                             )}
-                                                        </div>
                                                     </div>
                                                 </div>
-                                                <Modal show={data[index].map} onHide={() => this.closeMap(index)} dialogClassName="map-modal">
-                                                    <Modal.Body>
-                                                        <div style={{ position: 'absolute', right: 0, top: 0 }}>
-                                                            <MuiThemeProvider>
-                                                                <CloseIcon
-                                                                    onClick={() => this.closeMap(index)}
-                                                                    color="white"
-                                                                    style={{
-                                                                        padding: 0,
-                                                                        height: 32,
-                                                                        width: 32
-                                                                    }}
-                                                                />
-                                                            </MuiThemeProvider>
-                                                        </div>
-                                                        <div style={{backgroundColor: HeavyOrange, fontSize: "28pt", padding: 20, textAlign: "center" }}>MAP OF<br/>{item.title.toUpperCase()}</div>
-                                                        <ReactImageMagnify {...{
+                                            </div>
+                                            <Modal
+                                                show={data[index].map}
+                                                onHide={() =>
+                                                    this.closeMap(index)
+                                                }
+                                                dialogClassName="map-modal"
+                                            >
+                                                <Modal.Body>
+                                                    <div
+                                                        style={{
+                                                            position:
+                                                                'absolute',
+                                                            right: 0,
+                                                            top: 0
+                                                        }}
+                                                    >
+                                                        <MuiThemeProvider>
+                                                            <CloseIcon
+                                                                onClick={() =>
+                                                                    this.closeMap(
+                                                                        index
+                                                                    )
+                                                                }
+                                                                color="white"
+                                                                style={{
+                                                                    padding: 0,
+                                                                    height: 32,
+                                                                    width: 32
+                                                                }}
+                                                            />
+                                                        </MuiThemeProvider>
+                                                    </div>
+                                                    <div
+                                                        style={{
+                                                            backgroundColor: HeavyOrange,
+                                                            fontSize: '28pt',
+                                                            padding: 20,
+                                                            textAlign: 'center'
+                                                        }}
+                                                    >
+                                                        MAP OF<br />
+                                                        {item.title.toUpperCase()}
+                                                    </div>
+                                                    <ReactImageMagnify
+                                                        {...{
                                                             smallImage: {
                                                                 alt: `Map of ${item.title.toUpperCase()}`,
                                                                 isFluidWidth: true,
-                                                                src: item.mapImage
+                                                                src:
+                                                                    item.mapImage
                                                             },
                                                             largeImage: {
-                                                                src: item.mapImage,
-                                                                width: MAX_ZOOM_LEVEL * MAP_WIDTH,
-                                                                height: MAX_ZOOM_LEVEL * MAP_HEIGHT
+                                                                src:
+                                                                    item.mapImage,
+                                                                width:
+                                                                    MAX_ZOOM_LEVEL *
+                                                                    MAP_WIDTH,
+                                                                height:
+                                                                    MAX_ZOOM_LEVEL *
+                                                                    MAP_HEIGHT
                                                             },
-                                                            enlargedImagePosition: 'over',
+                                                            enlargedImagePosition:
+                                                                'over',
                                                             isHintEnabled: true,
                                                             isActivatedOnTouch: true,
                                                             shouldHideHintAfterFirstActivation: false,
-                                                            hintTextMouse: "Long-Touch to Zoom"
-                                                        }} />
-                                                        <div style={{backgroundColor: "rgb(13,109,121)", color: "rgb(107,193,209)", padding: 5, textAlign: "center"}}>TAP OUTSIDE OF MAP TO CLOSE</div>
-                                                    </Modal.Body>
-                                                </Modal>
-                                            </div>
-                                        );
-                                    }
-                                )}
+                                                            hintTextMouse:
+                                                                'Long-Touch to Zoom'
+                                                        }}
+                                                    />
+                                                    <div
+                                                        style={{
+                                                            backgroundColor:
+                                                                'rgb(13,109,121)',
+                                                            color:
+                                                                'rgb(107,193,209)',
+                                                            padding: 5,
+                                                            textAlign: 'center'
+                                                        }}
+                                                    >
+                                                        TAP OUTSIDE OF MAP TO
+                                                        CLOSE
+                                                    </div>
+                                                </Modal.Body>
+                                            </Modal>
+                                        </div>
+                                    );
+                                })}
                             </div>
                         </div>
                     </div>
@@ -238,7 +313,11 @@ class MapList extends React.Component {
                         }}
                         onClick={this.goDown}
                     >
-                        <img src={DownButton} style={{ width: '5%' }} alt="Down" />
+                        <img
+                            src={DownButton}
+                            style={{ width: '5%' }}
+                            alt="Down"
+                        />
                     </div>
                 </div>
             </div>
@@ -250,6 +329,6 @@ const mapStateToProps = ({ map }) => {
     const { maps } = map;
     return {
         maps
-    }
-}
-export default connect(mapStateToProps, null)(MapList)
+    };
+};
+export default connect(mapStateToProps, null)(MapList);
