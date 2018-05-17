c0844916c8277d4894e1f8d9a0e46c1eac683a42
Neal Shen <>
2018-05-17 15:18:40 +1000

    added animiation for rest mode

--- src/RestMode/RestComponent.js
+++ src/RestMode/RestComponent.js
@@ -3,14 +3,20 @@ import RestVideo from './RestVideo';
 import RestStaticAds from './RestStaticAds';
 import RestInternalAds from './RestInternalAds';
 
-export const RestComponent = (props) => {
-    return(
-        <div style={{height: "100%"}}>
-            <div style={{height: "12%"}}><RestInternalAds /></div>
-            <div style={{height: "43%"}}><RestVideo /></div>
-            <div style={{height: "45%"}}><RestStaticAds /></div>
+export const RestComponent = props => {
+    return (
+        <div style={{ height: '100%' }} className="section--rotate--animation">
+            <div style={{ height: '12%' }}>
+                <RestInternalAds />
+            </div>
+            <div style={{ height: '43%' }}>
+                <RestVideo />
+            </div>
+            <div style={{ height: '45%' }}>
+                <RestStaticAds />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default RestComponent;
